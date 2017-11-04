32ab47eb2708d6b25247c380df51c672f0e5b75b
jnlantos <jnlantos@gmail.com>
2017-11-04 19:00:10 -0400

    fixed the gitignore

--- Dune/Assets/ReadME!!!!!.cs
+++
@@ -1,4 +0,0 @@
-﻿/*
- * kk so the game will be based on grid cells. Each cell should be 1 unit wide.
- * When drawing pixels, we do 64/32 really doesnt matter pix per unit. Just make sure it has reasonable size.
- */
