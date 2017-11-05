19cdeca2fdd151bce1b8f84295530a5e11df7cdb
FreakingBarbarians <Paarthurnax@live.ca>
2017-11-04 21:33:41 -0400

    temp commit so i can continue on my desktop

--- Dune/Assets/Code/Base/GameEntities/Worm.cs
+++ Dune/Assets/Code/Base/GameEntities/Worm.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Worm : GridBehaviour {
-
 	[Range(0,1)]
 	public float moveTime;
 
@@ -16,7 +15,37 @@ public class Worm : GridBehaviour {
 
 	private int mode = 0;
 
+	private bool moving;
+
 	void Start() {
-				
+		
+	}
+
+	void Update(){
+		if (timer <= 0) {
+			moveRandom ();			
+		}
+	}
+
+	private void moveRandom(){
+		int go = UnityEngine.Random.Range (-1, 2);
+
+		if (xPos == 0) {
+			go = 1;
+		} else if (xPos + size < GridManager.instance.getGameSize() - 1) {
+			go = -1;
+		}
+
+		if(xPos - originalPos >= size/2) {
+			go = -1;
+		} else if (xPos - originalPos <= -size/2) {
+			go = 1;
+		}
+
+		newPos = transform.position + new Vector3 (go, 0, 0);
+		xPos += go;
+
+		
+
 	}
 }
